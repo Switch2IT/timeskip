4dc0d539be3c078a7f6756e97fa9e44e9d093fd1
ChristopheDevos <christophedevos_666@hotmail.com>
2017-05-19 12:09:29 +0200

    DateUtilsTest updated

--- timeskip-ejb/src/test/java/be/ehb/utils/DateUtilsTest.java
+++ timeskip-ejb/src/test/java/be/ehb/utils/DateUtilsTest.java
@@ -1,8 +1,12 @@
 package be.ehb.utils;
 
 import be.ehb.exceptions.InvalidDateException;
-import org.junit.*;
 import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 
@@ -13,7 +17,7 @@ public class DateUtilsTest {
 
     @Test
     public void convertStringToDate() throws Exception {
-        String dateToParse = "05-10-2017";
+        String dateToParse = "2017-10-05";
         Date date = new GregorianCalendar(2017, Calendar.OCTOBER,05).getTime();
         LocalDate parsedLocalDate =  LocalDate.fromDateFields(date);
         Assert.assertEquals(parsedLocalDate, DateUtils.convertStringToDate(dateToParse));
@@ -21,7 +25,7 @@ public class DateUtilsTest {
 
     @Test(expected = InvalidDateException.class)
     public void convertStringToDateWrongFormat() throws Exception {
-        String dateToParse = "2017-10-05";
+        String dateToParse = "05-10-2017";
         Date date = new GregorianCalendar(2017, Calendar.OCTOBER,05).getTime();
         LocalDate parsedLocalDate =  LocalDate.fromDateFields(date);
         DateUtils.convertStringToDate(dateToParse);
@@ -64,11 +68,6 @@ public class DateUtilsTest {
         Assert.assertEquals(expected, roundMiddle);
     }
 
-    @Test(expected = NullPointerException.class)
-    public void convertHoursToMinutesRoundnull() throws Exception {
-        long roundNull = DateUtils.convertHoursToMinutes(null);
-    }
-
     @Test
     public void convertHoursToMinutesRoundMax() throws Exception {
         long roundMiddle = DateUtils.convertHoursToMinutes((double) Long.MAX_VALUE);
@@ -86,12 +85,13 @@ public class DateUtilsTest {
     @Test
     public void getDatesBetween() throws Exception {
 
-        String startDate = "05-10-2017";;
-        String endDate = "07-10-2017";;
+        String startDate = "2017-10-05";
+        String endDate = "2017-10-07";
         List<Date> datesBetween = DateUtils.getDatesBetween(startDate,endDate);
-        List<LocalDate> datesBetweenManual = new ArrayList<>();
-        LocalDate start = DateUtils.convertStringToDate(startDate);
-        LocalDate between = DateUtils.convertStringToDate("06-10-2017");
+        List<Date> datesBetweenManual = new ArrayList<>();
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        Date start = format.parse(startDate);
+        Date between = format.parse("2017-10-06");
         datesBetweenManual.add(start);
         datesBetweenManual.add(between);
         Assert.assertArrayEquals(datesBetween.toArray(),datesBetweenManual.toArray());
@@ -102,16 +102,16 @@ public class DateUtilsTest {
 
     @Test(expected = InvalidDateException.class)
     public void getDatesBetweenWrongDate() throws Exception {
-        String startDate = "05-10-2017";;
-        String endDate = "13-13-2017";;
+        String startDate = "05-10-2017";
+        String endDate = "13-13-2017";
         List<Date> datesBetween = DateUtils.getDatesBetween(startDate,endDate);
     }
 
     @Test
     public void getDatesBetweenStartAfterEnddate() throws Exception {
 
-        String endDate = "05-10-2017";;
-        String startDate = "07-10-2017";;
+        String endDate = "2017-10-05";
+        String startDate = "2017-10-07";
         List<Date> datesBetween = DateUtils.getDatesBetween(startDate,endDate);
         List<Date> datesBetweenManual = new ArrayList<>();
         Assert.assertArrayEquals(datesBetween.toArray(),datesBetweenManual.toArray());
@@ -119,7 +119,7 @@ public class DateUtilsTest {
 
     @Test
     public void convertDateToString() throws Exception {
-        String parsedDate = "05-10-2017";
+        String parsedDate = "2017-10-05";
         Date date = new GregorianCalendar(2017, Calendar.OCTOBER,05).getTime();
         LocalDate localDateToParse =  LocalDate.fromDateFields(date);
         Assert.assertEquals(parsedDate,DateUtils.convertDateToString(localDateToParse));
